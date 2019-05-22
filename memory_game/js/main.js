f6c5fe4998dac1d073ba94c3789adbf1c24a74e9
Ruby King <ruby.sz.king@gmail.com>
2019-05-21 20:17:14 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,13 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardThree)
+var cards = ["queen", "queen", "king", "king", "cardOne", "cardTwo"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay [1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
